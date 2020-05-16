de4da6a3501471b6117fa122bc2b328675ee4077
Conner Ward <ckward@uci.edu>
2020-05-16 14:28:19 -0700

    update

--- google.py
+++ google.py
@@ -3,18 +3,17 @@ import datetime
 import requests
 import json
 
-start_date = 1577836800000000000
-end_date = 1585699200000000000
-curr_date = datetime.date.today().strftime("%d/%m/%Y")
-curr_time_ns = time.time_ns()
+# Default Info
+_START_DATE = 1577836800000000000
+_END_DATE = 1585699200000000000
+_CURR_DATE = datetime.date.today().strftime("%d/%m/%Y")
+_CURR_TIME_NS = time.time_ns()
+_DATA_SOURCE_ID = "derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"
+_DATASET_ID = f"{_START_DATE}-{_END_DATE}"
+_TOKEN = "ya29.a0Ae4lvC34Tel0ez6li8trnLxnYbTpvKg-5hQd8Z-XSc-wkzPQmf6WVVeS6gLntO5lThHJQmnp7seoYIUalOfLClljw6Nh" \
+         "-c4FMo4IQRu4CzQ2YotlRUtXBVPkXorTYLBUpg66xooJsfxgK2lrsiQqMjxKSQbongcigng "
 
-start_date = "asd"
-end_date = "asd"
-dsi = "derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"
-dsid = f"{start_date}-{end_date}"
-act = "ya29.a0Ae4lvC34Tel0ez6li8trnLxnYbTpvKg-5hQd8Z-XSc-wkzPQmf6WVVeS6gLntO5lThHJQmnp7seoYIUalOfLClljw6Nh-c4FMo4IQRu4CzQ2YotlRUtXBVPkXorTYLBUpg66xooJsfxgK2lrsiQqMjxKSQbongcigng"
-
-def google_request(access_token=act, dataSourceId=dsi, datasetId=dsid):
+def google_request(access_token=_TOKEN, dataSourceId=_DATA_SOURCE_ID, datasetId=_DATASET_ID):
     url = f'https://www.googleapis.com/fitness/v1/users/me/dataSources/{dataSourceId}/datasets/{datasetId}'
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"}
     result = requests.get(url, headers=headers)
@@ -25,7 +24,6 @@ def google_request(access_token=act, dataSourceId=dsi, datasetId=dsid):
 
     with open("date.txt", "w") as f:
         json.dump(json.dumps(json.loads(result.text), indent=1), f)
-
     above_1k = list()
     for i in range(0, len(db["point"])):
         if db["point"][i]["value"][0]["fpVal"] > 1000:
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import google
 
 web_hook = "https://discordapp.com/api/webhooks/708841556571848725/zJoGnm5jKbS6y6WRf5xCUKB4fIo7v0L2VBzemRa6pGkoeBYwzLgd9IhQ75cUeqGkLC6q"
 data = {"content":"Text"}
