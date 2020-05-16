34b25cc92d42314f0d4068877cae795661d4e134
Conner Ward <ckward@uci.edu>
2020-05-16 14:10:43 -0700

    innit

---
+++ discordbot.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+def discord_send(data, webhook_url):
+    """
+    Used to send data to a discord webhook url
+    :param data: dictionary of REST JSON. Key "contents" for message text
+    :param webhook_url: url of discord webhook
+    :return: None
+    """
+    dis_data = data
+    url = webhook_url
+    headers = {"Content-Type": "application/json"}
+    discord_request = requests.post(url, data=json.dumps(dis_data), headers=headers)
+
+    try:
+        discord_request.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+       print("Payload delivered successfully, code {}.".format(discord_request.status_code))
