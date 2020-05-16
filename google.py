8dd045a33d02b36eade27b250e8ff05519d0e7b9
Conner Ward <ckward@uci.edu>
2020-05-16 15:49:59 -0700

    asd

--- google.py
+++ google.py
@@ -4,16 +4,27 @@ import requests
 import json
 
 # Default Info
+# Date Range of Results
 _START_DATE = 1577836800000000000
 _END_DATE = 1585699200000000000
+# Current Date
 _CURR_DATE = datetime.date.today().strftime("%d/%m/%Y")
 _CURR_TIME_NS = time.time_ns()
+# Google Data
 _DATA_SOURCE_ID = "derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"
 _DATASET_ID = f"{_START_DATE}-{_END_DATE}"
+# Google Access Token
 _TOKEN = "ya29.a0Ae4lvC34Tel0ez6li8trnLxnYbTpvKg-5hQd8Z-XSc-wkzPQmf6WVVeS6gLntO5lThHJQmnp7seoYIUalOfLClljw6Nh" \
          "-c4FMo4IQRu4CzQ2YotlRUtXBVPkXorTYLBUpg66xooJsfxgK2lrsiQqMjxKSQbongcigng"
 
 def google_request(access_token=_TOKEN, dataSourceId=_DATA_SOURCE_ID, datasetId=_DATASET_ID):
+    """
+    Google Request for certain Data Source over time
+    :param access_token: From Google Oath2
+    :param dataSourceId: Location and Name of 
+    :param datasetId: in form f"{_START_DATE}-{_END_DATE}"
+    :return: (not currently working sends form of list)
+    """
     url = f'https://www.googleapis.com/fitness/v1/users/me/dataSources/{dataSourceId}/datasets/{datasetId}'
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {access_token}"}
     result = requests.get(url, headers=headers)
@@ -29,3 +40,5 @@ def google_request(access_token=_TOKEN, dataSourceId=_DATA_SOURCE_ID, datasetId=
         if db["point"][i]["value"][0]["fpVal"] > 1000:
             above_1k.append(db["point"][i]["value"][0]["fpVal"])
     print(int(sum(above_1k)))
+
+    return above_1k
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import discord
 import google
 
+# Get Token From token.txt
+with open("token.txt") as t:
+    for line in t:
+        TEMP_TOKEN = line
+
+# Request Response from Google (Check google.py for more specific variables and data sources)
+g_response = google.google_request(access_token=TEMP_TOKEN)
+
+# Send to Discord
 web_hook = "https://discordapp.com/api/webhooks/708841556571848725/zJoGnm5jKbS6y6WRf5xCUKB4fIo7v0L2VBzemRa6pGkoeBYwzLgd9IhQ75cUeqGkLC6q"
-data = {"content":"Text"}
+data = {"content":f"{str(g_response)}"}
 discord.discord_send(data=data, webhook_url=web_hook)
+
+
 
 
