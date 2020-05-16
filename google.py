1dbb2a1c7806eb70658cd101fa9cba2269b25570
Conner Ward <ckward@uci.edu>
2020-05-16 14:07:32 -0700

    Innit

---
+++ google.py
@@ -0,0 +1,7 @@
+
+dsi = "derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"
+dsid = f"{this_year_start_date}-{winter_end_date}"
+
+
+def google_request(dataSourceId=dsi, datasetId=dsid):
+    pass
