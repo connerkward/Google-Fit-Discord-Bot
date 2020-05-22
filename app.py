b79a48f131fe1108d0973f44c8953dbbbd90d998
Conner Ward <ckward@uci.edu>
2020-05-22 12:28:36 -0700

    proper css website

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask, url_for, session
 from flask import render_template, redirect
 from authlib.integrations.flask_client import OAuth
 import google
-import webhook
 import datetime
 import threading
-import starbot
-import _sqlite3
+import sqlite3
 
 # Discord Config
-web_hook_url = "https://discordapp.com/api/webhooks/708841556571848725/zJoGnm5jKbS6y6WRf5xCUKB4fIo7v0L2VBzemRa6pGkoeBYwzLgd9IhQ75cUeqGkLC6q"
 # Date Time Config
 _CURR_DATE = datetime.datetime.today()
 _CURR_DAY_START = _CURR_DATE.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -43,15 +40,15 @@ oauth.register(
 # Threading initialization might go here
 # One thread for the listening discord bot
 content1 = {'content': 'Hello from bot1'}
-t3 = threading.Thread(target=starbot.run())
-t3.start()
+#t3 = threading.Thread(target=starbot.run())
+#t3.start()
 
 # Flask Routes
 @app.route('/')
 def homepage():
     # Start Flask user 'Session'
     user = session.get('user')
-    return render_template('home.html', user=user)
+    return render_template('index.html', user=user)
 
 
 @app.route('/login')
@@ -75,7 +72,7 @@ def auth():
     g_response = google.request(access_token=token["access_token"], dataSourceId=sourceid, start=_CURR_DAY_START_NS,
                                 end=_CURR_DAY_END_NS)
     # STORE IN DATABASE, user, response
-
+    print(g_response)
     # STORE IN DATABASE
 
     #print(user['name'])
@@ -91,10 +88,3 @@ def auth():
 def logout():
     session.pop('user', None)
     return redirect('/')
-
-
-if __name__ == "__main__()":
-    print("hello")
-    content1 = {'content': 'Hello from bot1'}
-    t3 = threading.Thread(target=starbot.run())
-    t3.start()
